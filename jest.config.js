6eac305fab8f655b23034f66a5df376cb5f3bd49
JowayWang <joway.w@gmail.com>
2020-07-29 17:47:14 +0800

    chore

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,5 @@ module.exports = {
   transform: { '^.+\\.ts?$': 'ts-jest' },
   testEnvironment: 'node',
   testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFiles: ["jest-localstorage-mock"]
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 }
